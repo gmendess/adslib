0be8f82c843ede6411b6977e995d433ad247868b
Gabriel Mendes <gabriel.mendespng@gmail.com>
2020-11-10 20:30:48 -0300

    Implements ads_dlist_init and ads_dlist_add_* functions

---
+++ src/dlist/dlist.c
@@ -0,0 +1,106 @@
+#include "dlist.h"
+#include <stdlib.h>
+#include <memory.h>
+
+void ads_dlist_init(ads_dlist_t* dlist, void (*destroy)(void*)) {
+  dlist->destroy = destroy;
+  dlist->head    = NULL;
+  dlist->tail    = NULL;
+  dlist->size    = 0;
+}
+
+static inline 
+ads_dlist_node_t* ads_dlist_new_node(void* data) {
+  ads_dlist_node_t* new_node = malloc(sizeof(ads_dlist_node_t));
+  if(new_node) {
+    new_node->data = data;
+    new_node->next = NULL;
+    new_node->prev = NULL;
+  }
+
+  return new_node;
+}
+
+int ads_dlist_add_front(ads_dlist_t* dlist, void* data) {
+  ads_dlist_node_t* new_node = ads_dlist_new_node(data);
+  if(new_node == NULL)
+    return -1;
+
+  if(ads_dlist_is_empty(dlist)) {
+    dlist->head = new_node;
+    dlist->tail = new_node;
+  }
+  else {
+    dlist->head->prev = new_node;
+    new_node->next = dlist->head;
+    dlist->head = new_node;
+  }
+
+  dlist->size++;
+
+  return 0;
+}
+
+int ads_dlist_add_back(ads_dlist_t* dlist, void* data) {
+  ads_dlist_node_t* new_node = ads_dlist_new_node(data);
+  if(new_node == NULL)
+    return -1;
+  
+  if(ads_dlist_is_empty(dlist)) {
+    dlist->head = new_node;
+    dlist->tail = new_node;
+  }
+  else {
+    new_node->prev = dlist->tail;
+    dlist->tail->next = new_node;
+    dlist->tail = new_node;
+  }
+
+  dlist->size++;
+
+  return 0;
+}
+
+int ads_dlist_add_next(ads_dlist_t* dlist,
+                       ads_dlist_node_t* node,
+                       void* data)
+{
+  if(node == NULL)
+    return ads_dlist_add_front(dlist, data);
+  else if(node == ads_dlist_get_tail(dlist))
+    return ads_dlist_add_back(dlist, data);
+
+  ads_dlist_node_t* new_node = ads_dlist_new_node(data);
+  if(new_node == NULL)
+    return -1;
+
+  new_node->next = node->next;
+  new_node->prev = node;
+ 
+  node->next->prev = new_node;
+  node->next = new_node;
+
+  dlist->size++;
+  
+  return 0;
+}
+
+int ads_dlist_add_prev(ads_dlist_t* dlist,
+                       ads_dlist_node_t* node,
+                       void* data)
+{
+  if(node == ads_dlist_get_head(dlist) || node == NULL)
+    return ads_dlist_add_front(dlist);
+
+  ads_dlist_node_t* new_node = ads_dlist_new_node(data);
+  if(new_node == NULL)
+    return -1;
+
+  new_node->next = node;
+  new_node->prev = node->prev;
+
+  node->prev->next = new_node;
+  node->prev = new_node;
+
+  return 0;
+}
---
+++ src/dlist/dlist.h
@@ -0,0 +1,50 @@
+#ifndef ADS_DLINKED_LIST_H
+#define ADS_DLINKED_LIST_H
+
+#include <stdlib.h>
+
+/*
+  DOUBLE LINKED LIST HEADER
+*/
+
+// individual node in a double linked list
+typedef struct ads_dlist_node {
+  void* data;
+  struct ads_dlist_node* next;
+  struct ads_dlist_node* prev;
+} ads_dlist_node_t;
+
+// double linked list
+typedef struct ads_dlist {
+  ads_dlist_node_t* head;
+  ads_dlist_node_t* tail;
+  size_t size;
+
+  void (*destroy)(void* data);
+} ads_dlist_t;
+
+#define ads_dlist_get_size(dlist) ((dlist)->size)
+#define ads_dlist_get_head(dlist) ((dlist)->head)
+#define ads_dlist_get_tail(dlist) ((dlist)->tail)
+#define ads_dlist_is_empty(dlist) (ads_dlist_get_size((dlist)) == 0 ? 1 : 0)
+
+#define ads_dlist_get_next(node)  ((node)->next)
+#define ads_dlist_get_prev(node)  ((node)->prev)
+
+void ads_dlist_init(ads_dlist_t* dlist, void (*destroy)(void*));
+void ads_dlist_destroy(ads_dlist_t* dlist);
+void ads_dlist_clean(ads_dlist_t* dlist);
+
+int ads_dlist_remove_front(ads_dlist_t* dlist, void** ret_data);
+int ads_dlist_remove_back(ads_dlist_t* dlist, void** ret_data);
+int ads_dlist_remove_next(ads_dlist_t* dlist, ads_dlist_node_t* node, void** ret_data);
+int ads_dlist_remove_prev(ads_dlist_t* dlist, ads_dlist_node_t* node, void** ret_data);
+
+int ads_dlist_add_front(ads_dlist_t* dlist, void* data);
+int ads_dlist_add_back(ads_dlist_t* dlist, void* data);
+int ads_dlist_add_next(ads_dlist_t* dlist, ads_dlist_node_t* node, void* data);
+int ads_dlist_add_prev(ads_dlist_t* dlist, ads_dlist_node_t* node, void* data);
+
+ads_dlist_node_t* ads_dlist_get_at(ads_dlist_t* dlist, int index);
+
+#endif
